b725767106a0bfb61ae8a86050f2e10d8d8d988c
justrelax74 <43277078+justrelax74@users.noreply.github.com>
2025-02-15 16:47:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+  const menu = document.getElementById("navbarMenu");
+  menu.classList.toggle("active");
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     console.log("DOM fully loaded");
 
